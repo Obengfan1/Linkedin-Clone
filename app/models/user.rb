aa594c594beabdff6843d294553682351b11e09d
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-04-06 20:20:18 +0200

    more columns were added to experience model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   has_many :educations, dependent: :destroy
-  has_Many :experiences, dependent: :destroy
+  has_many :experiences, dependent: :destroy
   has_many :skills, dependent: :destroy
   has_many :posts, dependent: :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_165901) do
+ActiveRecord::Schema.define(version: 2022_04_06_181337) do
+
+  create_table "educations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_educations_on_user_id"
+  end
+
+  create_table "experiences", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "employment_type"
+    t.string "company_name"
+    t.string "location"
+    t.index ["user_id"], name: "index_experiences_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -18,6 +36,13 @@ ActiveRecord::Schema.define(version: 2022_04_06_165901) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_skills_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,5 +58,8 @@ ActiveRecord::Schema.define(version: 2022_04_06_165901) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "educations", "users"
+  add_foreign_key "experiences", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "skills", "users"
 end
