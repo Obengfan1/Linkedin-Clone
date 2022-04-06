ad312636fe56f9fbedf5ab3f41478230f992cb31
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-04-06 19:12:32 +0200

    added association between user and its education, experiences and skills

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :educations
+  has_Many :experiences
+  has_many :skills
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
