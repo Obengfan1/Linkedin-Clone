7a24b434cc8b90660e06df17f4dfec880eeff222
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-04-15 16:25:00 +0200

    created the new post features

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_04_06_210939) do
 ActiveRecord::Schema.define(version: 2022_04_12_123155) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
