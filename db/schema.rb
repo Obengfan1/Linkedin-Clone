902ab044444b67e63aa8bc8f0486644d4acd465a
swiches <donasare@live.com>
2022-04-15 13:40:16 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_04_06_210939) do
 ActiveRecord::Schema.define(version: 2022_04_12_123155) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -36,6 +33,8 @@ ActiveRecord::Schema.define(version: 2022_04_12_123155) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "school"
+    t.string "degree"
+    t.string "field_of_study"
     t.index ["user_id"], name: "index_educations_on_user_id"
   end
 
