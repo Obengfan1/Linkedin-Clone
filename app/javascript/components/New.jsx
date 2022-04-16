a8617bb48ce33fa459cb11084f04300bd622727d
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-04-16 21:10:07 +0200

    routes and post controller modified

--- app/javascript/components/New.jsx
+++ app/javascript/components/New.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
-const NewPost = ({ post }) => {
+const NewPost = ({ post, authenticity_token }) => {
   return (
-
     <div className="container">
       <div>
-         <form method="POST" action="">
+         <form method="POST" action="/posts">
            <input
               type = "text"
               placeholder="post"
            />
            <button type = "submit">Post</button>
+
+           <input
+            name="authenticity_token"
+            type="hidden"
+            value={authenticity_token}
+          />
+
          </form>
-
       </div>
-
-
     </div>
   )
 }
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :messages, only: [:create]
   end
 
-  root 'posts#new'
+  root 'posts#new', as: :post
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
